be74ff0c1df76ff7a60de2e35ce8358015d388ff
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2023-11-25 18:36:31 +0100

    crc32: use correct hash function
    Signed-off-by: Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>

--- crc32.cpp
+++ crc32.cpp
@@ -2,5 +2,5 @@
 
 auto main(int argc, char *argv[]) -> int {
     const auto fd = raii::open(argv[1], O_RDONLY);
-    return print_and_compute<::adler32>(fd);
+    return print_and_compute<::crc32>(fd);
 }
